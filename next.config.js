1e698e03c66fddf9f7c807228657196604b13ee8
zpuckeridge <zacchary@puckeridge.me>
2023-02-21 23:57:19 +0000

    fix: config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["flowbite.s3.amazonaws.com"],
+    unoptimized: true,
   },
   basePath: "/savvyelectricalanddata.com.au",
 };
