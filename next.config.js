1974ae48d2d3c94d82ed920742b0a476295bfec2
zpuckeridge <zacchary@puckeridge.me>
2023-02-22 00:56:16 +0000

    fix: language attribute

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body className="max-w-6xl mx-auto">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
