b4751926fd723e41b35a6e1a919100ac3bb4760b
zpuckeridge <zacchary@puckeridge.me>
2023-02-22 00:16:30 +0000

    fix: deployment workflow

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+  assetPrefix: isProd ? "/savvyelectricalanddata.com.au/" : "",
   reactStrictMode: true,
   swcMinify: true,
   images: {
     unoptimized: true,
   },
-  basePath: "/savvyelectricalanddata.com.au",
 };
 
 module.exports = nextConfig;
