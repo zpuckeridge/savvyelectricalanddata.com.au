fb00a641ae102992f4b9549ff9716231f73d4d79
Zacchary Puckeridge <zacchary@puckeridge.me>
2023-03-09 15:43:42 +1000

    feat: analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script form "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <Script defer data-domain="savvyelectricalanddata.com.au" src="https://analytics.speedy.af/js/script.js" />
       <body className="max-w-6xl mx-auto">
         <Main />
         <NextScript />
